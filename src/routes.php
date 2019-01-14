d672bafc877b54d55f9ed1800e8684b8f34ac10c
David Câmara <davidcamara21@hotmail.com>
2019-01-14 03:19:41 +0000

    up

--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,11 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 
 // Routes
+$app->get('/', function (Request $request, Response $response, array $args) {
+
+    return $this->renderer->render($response, 'index.html');
+});
+
 
 $app->get('/test/near', function (Request $request, Response $response, array $args) {
 
