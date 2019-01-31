a4f480bc62e10f813769f364bfc8ea402dc235bd
David Câmara <davidcamara21@hotmail.com>
2019-01-31 16:20:42 +0000

    comments

--- src/routes.php
+++ src/routes.php
@@ -5,28 +5,19 @@ use Slim\Http\Response;
 
 // Routes
 $app->get('/', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    //$this->logger->info("Slim-Skeleton '/' route");
-    // Render index view
     return $this->renderer->render($response, 'index.html');
 });
 
 $app->get('/test/map', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    //$this->logger->info("Slim-Skeleton '/' route");
-    // Render index view
     return $this->renderer->render($response, 'createTrip.php');
 });
 
 $app->get('/test/near', function (Request $request, Response $response, array $args) {
-
     $point = $request->getParam("point");
-
     //get json from the indiecamper API
     $nearPlace = json_decode(file_get_contents("https://indiecamper.azurewebsites.net/near?point=$point"));
 
     $latlong = explode(",", $point);
-
     $lat = $nearPlace->latitude;
     $long = $nearPlace->longitude;
     $name = $nearPlace->name;
